5a07bba76fe43920ce719456370de261c78e7c97
Nasrin Hasanpour <n75hasanpur@gmail.com>
2025-12-27 17:33:41 +0330

    add role

---
+++ Controllers/RoleController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace salian_front.Controllers
+{
+    public class RoleController : Controller
+    {
+        public ActionResult List()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+}
+}

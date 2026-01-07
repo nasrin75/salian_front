bdc2791f890862835fbe60e5a348b21cc26e36a5
Nasrin Hasanpour <n75hasanpur@gmail.com>
2026-01-07 16:03:57 +0330

    add EqipmentFeature part

---
+++ Controllers/FeatureController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace salian_front.Controllers
+{
+    public class FeatureController : Controller
+    {
+        public IActionResult List()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
